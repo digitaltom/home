41d9b80bcd81fa28e0a1778a31e4b15806c92999
Thomas Schmidt <tschmidt@suse.de>
2020-06-03 00:26:29 +0200

    fix redirect

--- userscripts/jira-sd-redirect-to-admin-interface.user.js
+++ userscripts/jira-sd-redirect-to-admin-interface.user.js
@@ -5,20 +5,18 @@
 // @description  The admin view is much more powerful to work on tickets
 // @author       Thomas Schmidt
 // @run-at       document-start
-// @match        https://sd.suse.com.com/servicedesk/*
+// @match        https://sd.suse.com/servicedesk/*
 // @grant        none
 // @downloadURL  https://github.com/digitaltom/home/raw/master/userscripts/jira-sd-redirect-to-admin-interface.user.js
 // @updateURL    https://github.com/digitaltom/home/raw/master/userscripts/jira-sd-redirect-to-admin-interface.user.js
 // ==/UserScript==
 
-(function() {
-    'use strict';
 
-    var ticket = location.href.split('/')[location.href.split('/').length - 1 ];
-    var target = "https://sd.suse.com/browse/" + ticket;
+var ticket = location.href.split('/')[location.href.split('/').length - 1 ];
+var target = "https://sd.suse.com/browse/" + ticket;
 
-    document.write("<br><h2>Redirecting to the admin view...</h2><br><p>" + target + "</p>");
+var log = "Redirecting to the jira-sd admin view at " + target;
+console.log(log);
 
-    window.location.replace(target);
-})();
+location.replace(target);
 
